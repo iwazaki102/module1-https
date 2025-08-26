02244f6c9160112b0c52ed8bd6393caaeb532c12
iwazaki102 <indra.wibawa.400@gmail.com>
2025-08-26 14:28:28 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,11 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+// Jika pakai Tailwind via build, pastikan import CSS utama:
+// import "./index.css";
 
-createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
